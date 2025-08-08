c7bd4cdcedcda41a12aa333f7bfc6eb810a856d3
Jaslin Reena <jaslin@fscmfactory.com>
2025-08-08 05:20:11 +0000

    Addition

--- sample_project/srv/service.cds
+++ sample_project/srv/service.cds
@@ -1,3 +1,5 @@
 service product {
    function printhelloworld (input: String )returns String;
+
+   function addition (num1: Integer, num2: Integer)returns Integer;
 } 
--- sample_project/srv/service.js
+++ sample_project/srv/service.js
@@ -6,6 +6,12 @@ module.exports = cds.service.impl(async function(srv) {
         console.log(req.data.input)
         return `${req.data.input} World`
     })
-    
+   
+    srv.on('addition', req => {
+        console.log(req.data)
+        let reslut = req.data.num1 + req.data.num2;
+        return reslut
+    })
+
 })
 
